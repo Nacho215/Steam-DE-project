9387f3d2e52ac66b1d100f5b8e6c13f196c77279
Juan Ignacio Hernández <hernandezjuanvt@gmail.com>
2023-03-20 20:52:44 -0300

    docs: Added comments and docstrings on scrapper script

--- src/steamspy_scrapper.py
+++ src/steamspy_scrapper.py
@@ -37,77 +37,106 @@ async def get_app_details(session, appid):
     Returns:
         dict: App details in a JSON format.
     """
-    # Build url and
+    # Build url and make async request
     url = f'{API_BASE_URL}?request=appdetails&appid={appid}'
     async with session.get(url) as r:
+        # Raise exception if any
         if r.status != 200:
             r.raise_for_status()
+        # Wait for response and returns it in JSON format
         json_data = await r.json()
         if json_data:
             return json_data
         else:
+            # IF can't scrape this app, return only appid
             print(f"Can't scrape info for appid: {appid}")
             return {"appid": [str(appid)]}
 
 
 async def collect_all_app_details(session, app_ids_list):
+    """
+    Given a list of app ids, requests SteamSpy API for those app details.
+
+    Args:
+        session (aiohttp.ClientSession): Session used for async http requests.
+        app_ids_list (list): List of Steam app IDs to get details.
+
+    Returns:
+        list: A list of dictionaries, containing app details.
+    """
+    # Create an async task for each app in the list
     tasks = []
     for app_id in app_ids_list:
         task = asyncio.create_task(get_app_details(session, app_id))
         tasks.append(task)
-    res = await asyncio.gather(*tasks)
-    # Remember apps scrapped
+    # Wait for all tasks and collect results
+    results = await asyncio.gather(*tasks)
+    # Count apps scrapped for logging
     global total_apps_scrapped
     total_apps_scrapped = total_apps_scrapped + len(app_ids_list)
     print(f"Scrapped {len(app_ids_list)} apps. Progress: {total_apps_scrapped}/{total_apps}")
-    return res
+    # Return details for all apps in the list
+    return results
 
 
 def store_app_details(app_details, csv_path, fieldnames):
+    """
+    Store given app details into a csv file with given columns.
+
+    Args:
+        app_details (list): A list of dictionaries containing app details.
+        csv_path (str): Path for store csv file.
+        fieldnames (list): A list of fields to filter.
+    """
     with open(csv_path, "a", encoding='utf8') as app_data_file:
-        # get dictionary keys for the CSV header
         file_writer = csv.DictWriter(
-            app_data_file, fieldnames=fieldnames, extrasaction='ignore')
+            app_data_file,
+            fieldnames=fieldnames,
+            extrasaction='ignore'
+        )
         file_writer.writerows(app_details)
 
 
-async def main():
-
-    # # Prepare csv file
-    # with open(PATH_CSV_APP_DATA, 'w', newline='') as f:
-    #     writer = csv.DictWriter(f, fieldnames=steam_columns)
-    #     writer.writeheader()
-
-    # Get app ids from csv
+async def scrape_steamspy_data():
+    """
+    Main function. Scrape all games data using SteamSpy API and store
+    results in a csv file.
+    """
+    # Prepare csv file
+    with open(PATH_CSV_APP_DATA, 'w', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=STEAMSPY_COLUMNS)
+        writer.writeheader()
+    # Get app ids from csv and make it a list
     app_list = pd.read_csv(PATH_CSV_APP_LIST)
-
-    # Get only appids and make it a list
     app_list = list(app_list["appid"])
-
     # TEST. Get only some apps for debug
     # app_list = app_list[0:DEBUG_APP_QUANTITY]
 
-    # Remember total apps to scrape
+    # Calculate total apps to scrape for logging
     global total_apps
     total_apps = len(app_list) - DEBUG_START_INDEX
 
-    game_details = []
-
+    # Initialize empty list
+    app_details = []
+    # Divide all app list into batches
     batch_limits = np.arange(DEBUG_START_INDEX, len(app_list), BATCH_SIZE)
-
     for i in batch_limits:
+        # Get and store app details for each batch
         batch = app_list[i: i+BATCH_SIZE]
         async with aiohttp.ClientSession() as session:
-            game_details = await collect_all_app_details(session, batch)
-        store_app_details(game_details, PATH_CSV_APP_DATA, STEAMSPY_COLUMNS)
-        # Don't wait at the end
+            app_details = await collect_all_app_details(session, batch)
+        store_app_details(app_details, PATH_CSV_APP_DATA, STEAMSPY_COLUMNS)
+        # Wait between requests to avoid overloading the API
         if i < batch_limits[-1]:
             print(f"Waiting {WAIT_TIME} seconds...")
             time.sleep(WAIT_TIME)
 
 
 if __name__ == '__main__':
+    # Start time
     start = time.perf_counter()
-    asyncio.run(main())
+    # Scrape steamspy data
+    asyncio.run(scrape_steamspy_data())
+    # Log total time taken
     stop = time.perf_counter()
-    print("time taken:", stop - start)
+    print("Total time taken:", stop - start)
