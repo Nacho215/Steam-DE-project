062df6790008acd22c78427af49a6f972435d35b
Juan Ignacio Hernández <hernandezjuanvt@gmail.com>
2023-04-19 16:25:33 -0300

    fix: added pool_pre_ping strategy to db.py to avoid conection errors

--- libs/db.py
+++ libs/db.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.dirname(__file__))
 from settings import settings
 
 # Engine is created to be called as modules from other scripts
-default_engine = create_engine(settings.DATABASE_URL)
+default_engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
 
 # Logging config
 # logging.config.fileConfig('config_logs.conf')
